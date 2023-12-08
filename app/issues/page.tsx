8e4859c8288517a9082388749749fb213e632317
Seth Ketsi <christambassador12@gmail.com>
2023-12-08 12:57:14 +0000

    New Issue Page Built

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Button } from '@radix-ui/themes'
+import Link from 'next/link'
 
 const IssuesPage = () => {
   return (
     <div>
       <h1>IssuesPage</h1>
-      <Button>New Issue</Button>
+      <Button><Link href='/issues/new'>New Issues</Link></Button>
     </div>
   )
 }
